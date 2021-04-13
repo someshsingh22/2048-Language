b8d520989b0d701848eb1c475dc8007bf0e26f0a
someshsingh22 <someshsingh9414@gmail.com>
2021-04-13 20:55:59 +0530

    PARSE ERROR

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ from parser import Parser2048
 # from parser import Parser2048
 
 lexer = Lexer2048()
+parser = Parser2048()
 while True:
     command = input("2048> ")
 
     try:
-        tokens = lexer.err_tokenize(command)
+        tokens = lexer.tokenize(command)
         for token in tokens:
             print(token)
+        parsed = parser.parse(tokens)
+
     except Exception as E:
         print(str(E))
