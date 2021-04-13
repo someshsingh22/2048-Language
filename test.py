c62081ce94ea1adc38789b14a3c66809439f4e1c
someshsingh22 <someshsingh9414@gmail.com>
2021-04-14 04:12:44 +0530

    Final Lexer

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 from game import Board, Tile
 from lexer import Lexer2048
-from parser import Parser2048
 
 # from parser import Parser2048
 
 lexer = Lexer2048()
-parser = Parser2048()
+# parser = Parser2048()
 while True:
     command = input("2048> ")
 
@@ -13,7 +12,7 @@ while True:
         tokens = lexer.tokenize(command)
         for token in tokens:
             print(token)
-        parsed = parser.parse(tokens)
+        # parsed = parser.parse(tokens)
 
     except Exception as E:
         print(str(E))
