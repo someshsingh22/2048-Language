956a1177620469c505897c0ee4d1841351a0591a
someshsingh22 <someshsingh9414@gmail.com>
2021-04-13 07:43:50 +0530

    Basic Parser

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 from game import Board, Tile
 from lexer import Lexer2048
+from parser import Parser2048
+
+# from parser import Parser2048
 
 g = Board(size=(4, 4))
 g.matrix[1][1] = Tile(index=(1, 1), value=3)
 g.matrix[3][2] = Tile(index=(2, 1), value=1024)
 print(g)
 
-commands = ["""ADD LEFT""", """ASSIGN   (VALUE IN  3,8) TO 7,7""", """VAR 26 IS 5,8""", """VALUE IN 4,9"""]
-lexer = Lexer2048()
+commands = [
+    """ADD LEFT""",
+    """ASSIGN   (VALUE IN  3,8) TO 7,7""",
+    """VAR ABC IS 5,8""",
+    """VALUE IN 4,9""",
+]
+
+lexer, parser = Lexer2048(), Parser2048()
 for command in commands:
-    print(command)
-    print()
-    for tok in lexer.tokenize(command):
-        print(tok)
-    print()
+    parser.parse(lexer.tokenize(command))
+    print(parser.command)
