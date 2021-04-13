57aa29c918ba4ff449c795fd0e43b0f6cfef741e
someshsingh22 <someshsingh9414@gmail.com>
2021-04-14 04:43:06 +0530

    Basic Parser

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ from game import Board, Tile
 from lexer import Lexer2048
 from parser import Parser2048
 
-out = []
+
 lexer, parser = Lexer2048(), Parser2048()
+out = 0
 while True:
-    command = input("2048>")
-    command = lexer.err(command)
-    out = parser.parse(lexer.tokenize(command))
-    print(out)
+    try:
+        command = input("2048>")
+        command = lexer.err(command)
+        out = parser.parse(lexer.tokenize(command))
+        print(out)
+    except Exception as E:
+        print(str(E))
