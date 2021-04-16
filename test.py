d50ad00573942a88d7c02af28b93a1c02495d5fe
someshsingh22 <someshsingh9414@gmail.com>
2021-04-16 05:58:12 +0530

    colored

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ lexer, parser = Lexer2048(), Parser2048(fmap=board.fmap)
 
 while True:
     try:
-        print("2048 >>>", end=" ")
+        print("\033[33m" + "2048 >>>" + "\033[0m", end=" ")
         inp = input()
         command = lexer.preprocess(inp)
         out = parser.parse(lexer.tokenize(command))
